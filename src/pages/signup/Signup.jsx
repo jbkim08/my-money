f76f8a0a9095eacc666f2e2aab323ed3b1964d3c
JB <drv98@naver.com>
2023-12-04 11:13:07 +0900

    가입하기 기능 완료

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import styles from './Signup.module.css';
+import { useSignup } from '../../hooks/useSignup';
 
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
+  //미리 만든 가입하기훅으로 가입하기 함수와 에러, 로딩상태를 가져옴
+  const { signup, error, isPending } = useSignup();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, name);
+    signup(email, password, name);
   };
 
   return (
@@ -27,7 +30,13 @@ export default function Signup() {
         <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
       </label>
 
-      <button className="btn">가입하기</button>
+      {!isPending && <button className="btn">가입하기</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          가입중...
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 }
