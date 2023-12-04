c74b65c88331cb71e0b0183823f685e3ef70f325
JB <drv98@naver.com>
2023-12-04 10:19:55 +0900

    가입하기

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
 import styles from './Signup.module.css';
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, name);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Signup</h2>
+      <label>
+        <span>email:</span>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+      </label>
+      <label>
+        <span>password:</span>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+      </label>
+      <label>
+        <span>name:</span>
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      </label>
+
+      <button className="btn">가입하기</button>
+    </form>
+  );
 }
