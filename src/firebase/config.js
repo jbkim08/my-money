4916f214890323116ec1c1b6a9c3556eba777d4c
JB <drv98@naver.com>
2023-12-04 10:23:35 +0900

    파이어베이스 설정(db추가)

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxPcnzzWc5BVZQqCgNJUSZtgo3L71o3ZM',
+  authDomain: 'mymoney-drv98.firebaseapp.com',
+  projectId: 'mymoney-drv98',
+  storageBucket: 'mymoney-drv98.appspot.com',
+  messagingSenderId: '715620903888',
+  appId: '1:715620903888:web:2edc236f37d8fe059f67d5',
+};
+
+//본인 계정의 키등 설정값을 가지고 초기화
+firebase.initializeApp(firebaseConfig);
+
+//파이어스토어 DB 서비스
+const firedb = firebase.firestore();
+
+export { firedb };
