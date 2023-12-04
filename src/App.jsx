da213c62ef91d81cbd9778a43d7bcde8ec958be4
JB <drv98@naver.com>
2023-12-04 15:04:00 +0900

    네비게이션 가드

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
@@ -6,16 +6,16 @@ import Navbar from './components/Navbar';
 import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
       {authIsReady && (
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/login" element={!user ? <Login /> : <Home />} />
+            <Route path="/signup" element={!user ? <Signup /> : <Home />} />
           </Routes>
         </BrowserRouter>
       )}
