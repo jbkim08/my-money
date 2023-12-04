378019c64a68a00dcae47ee6522cde7dcdf43e0d
JB <drv98@naver.com>
2023-12-04 11:26:55 +0900

    인증컨텍스트 사용하기

---
+++ src/hooks/useAuthContext.js
@@ -0,0 +1,13 @@
+//인증컨텍스트를 사용하기 위한 훅 (반복 사용시 필요)
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw Error('useAuthContext는 AuthContextProvider 내에서만 사용가능합니다.');
+  }
+
+  return context;
+};
