6f595127ebdecad9908c98174349156f0eb10af0
JB <drv98@naver.com>
2023-12-05 09:36:33 +0900

    파이어스토어 입력하기

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
+import { useAuthContext } from '../../hooks/useAuthContext';
 import styles from './Home.module.css';
 import TransactionForm from './TransactionForm';
 
 export default function Home() {
+  const { user } = useAuthContext();
   return (
     <div className={styles.container}>
       <div className={styles.content}>거래 내역</div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
