af40e2b23f3994b1bf3935a39d42b2c7c4599b5d
JB <drv98@naver.com>
2023-12-05 10:13:42 +0900

    같은 유저의 비용만 보이게

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import TransactionForm from './TransactionForm';
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection('transactions');
+  const { documents, error } = useCollection('transactions', ['uid', '==', user.uid]);
   return (
     <div className={styles.container}>
       <div className={styles.content}>
