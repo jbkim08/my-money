2a60e37ffe63256fdd7577bbe0f85c773c2b5f94
JB <drv98@naver.com>
2023-12-05 09:49:09 +0900

    useCollection으로 실시간 비용표시

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 import styles from './Home.module.css';
+import TransactionList from './TransactinList';
 import TransactionForm from './TransactionForm';
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection('transactions');
   return (
     <div className={styles.container}>
-      <div className={styles.content}>거래 내역</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
